e489e40ab353edb23e0d41fa34d753979429aa4c
Adegbesan Joshua Temitope <112911898+joshua468@users.noreply.github.com>
2024-03-22 15:39:17 +0100

    Add files via upload

---
+++ RPC-server-client/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+)
+
+type Item struct {
+	title string
+	body  string
+}
+
+type API int
+
+var database []Item
+
+func (a *API) GetDB(title string, reply *[]Item) error {
+	*reply = database
+	return nil
+}
+func (a *API) GetByName(title string, reply *Item) error {
+	var getItem Item
+	for _, val := range database {
+		if val.title == title {
+			getItem = val
+		}
+	}
+	*reply = getItem
+	return nil
+
+}
+
+func Createitem(item Item) Item {
+	database = append(database, item)
+	return item
+}
+
+func (a *API) Additem(item Item, reply *Item) error {
+	database = append(database, item)
+	*reply = item
+	return nil
+}
+
+func (a *API) EditItem(edit Item, reply *Item) error {
+	var changed Item
+
+	for idx, val := range database {
+		if val.title == edit.title {
+			database[idx] = Item{edit.title, edit.body}
+			changed = database[idx]
+		}
+	}
+	*reply = changed
+	return nil
+
+}
+
+func (a *API) DeleteItem(item Item, reply *Item) error {
+	var del Item
+
+	for idx, val := range database {
+		if val.title == item.title && val.body == item.body {
+			database = append(database[:idx], database[idx+1:]...)
+			del = item
+			break
+
+		}
+	}
+	*reply = del
+	return nil
+}
+
+func main() {
+
+	var api = new(API)
+	err := rpc.Register(api)
+
+	if err != nil {
+		log.Fatal("error registering api", api)
+	}
+	rpc.HandleHTTP()
+
+	listener, err := net.Listen("tcp", ":4040")
+
+	if err != nil {
+		log.Fatal("listener error", err)
+	}
+	log.Printf("serving rpc on port %d", 4040)
+	err = http.Serve(listener, nil)
+	if err != nil {
+		log.Fatal("error serving: ", err)
+	}
+}
+
+/*fmt.Println("Initial database:", database)
+a := Item{"first", "a test item"}
+b := Item{"second", "a second item"}
+c := Item{"third", "a third item"}
+
+Additem(a)
+Additem(b)
+Additem(c)
+fmt.Println("second database", database)
+
+DeleteItem(b)
+fmt.Println("third item database", database)
+
+EditItem("third", Item{"fourth", "a new item"})
+fmt.Println("forth database", database)
+
+x := GetByName("fourth")
+y := GetByName("first")
+fmt.Println(x, y)
+
+*/
